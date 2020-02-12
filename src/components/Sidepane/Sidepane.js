5b656ef79696993b549d28bccd0bd0d7ddd0adfd
Michelle Pitts <michelle.m.pitts@gmail.com>
2020-02-12 10:31:08 -0500

    added navbar and sidepane

---
+++ src/components/Sidepane/Sidepane.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Sidepane.css'
+const Sidepane = () => (
+    <div className='sidepane'>
+        <h3>Q1</h3>
+        <h3>Q2</h3>
+        <h3>Summary</h3>
+    </div>
+)
+
+export default Sidepane
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import logo from '../../logo.svg';
 import './App.css';
 import NavBar from '../../components/Navbar/Navbar'
-
+import Sidepane from '../../components/Sidepane/Sidepane'
 function App() {
   return (
-    <>
+    <div className='page'>
     <NavBar />
-    </>
+    <Sidepane />
+    </div>
   );
 }
 
